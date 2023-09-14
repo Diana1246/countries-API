90728f966d8229462edf351b0ec47ca58e9a754c
Diana1246 <109847803+Diana1246@users.noreply.github.com>
2023-09-14 19:03:00 +0300

    Country

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const alertsContainer = document.getElementById('alerts-container');
 
 const API_URL = 'https://restcountries.com/v2/name';
 
-// Функція для відображення сповіщень за допомогою Bootstrap Alert
+ 
 function showAlert(message, type) {
     const alertElement = document.createElement('div');
     alertElement.classList.add('alert', `alert-${type}`, 'alert-dismissible', 'fade', 'show');
@@ -22,34 +22,34 @@ function showAlert(message, type) {
     
     setTimeout(() => {
         alertElement.remove();
-    }, 5000); // Закрити сповіщення автоматично через 5 секунд
+    }, 5000);  
 }
 
-// Функція для відображення даних про країну
-function renderCountryDetails(country) {
+ 
+function renderCountryies(country) {
     countryDetails.innerHTML = `
         <h2>${country.name}</h2>
         <p><strong>Столиця:</strong> ${country.capital}</p>
         <p><strong>Населення:</strong> ${country.population}</p>
-        <p><strong>Мови:</strong> ${country.languages.map((lang) => lang.name).join(', ')}</p>
+        <p><strong>Мови:</strong> ${country.languages}</p>
         <img src="${country.flags[0]}" alt="Прапор ${country.name}" width="100">
     `;
 }
 
-// Функція для відображення списку країн
+ 
 function renderCountryList(countries) {
     countryList.innerHTML = '';
     countries.forEach((country) => {
         const listItem = document.createElement('li');
         listItem.textContent = country.name;
         listItem.addEventListener('click', () => {
-            renderCountryDetails(country);
+            renderCountryies(country);
         });
         countryList.appendChild(listItem);
     });
 }
 
-// Функція для виконання HTTP-запиту з використанням debounce
+ 
 const searchCountry = debounce(async (query) => {
     try {
         const xhr = new XMLHttpRequest();
@@ -58,7 +58,7 @@ const searchCountry = debounce(async (query) => {
             if (xhr.status === 200) {
                 const countries = JSON.parse(xhr.responseText);
                 if (countries.length === 1) {
-                    renderCountryDetails(countries[0]);
+                    renderCountryies(countries[0]);
                 } else if (countries.length > 10) {
                     showAlert('Будь ласка, зробіть запит більш специфічним.', 'warning');
                 } else {
@@ -70,11 +70,11 @@ const searchCountry = debounce(async (query) => {
         };
         xhr.send();
     } catch (error) {
-        showAlert('Помилка при виконанні запиту.', 'danger');
+        showAlert('Помилка!!!', 'danger');
     }
 }, 500);
 
-// Обробник події input для пошуку країн
+ 
 input.addEventListener('input', (event) => {
     const query = event.target.value;
     if (query.trim() !== '') {
